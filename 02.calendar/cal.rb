f3b3d1d86cd25264cc6b9e05adb510b9f2a5678d
paulo0212 <rikuo.iwasaki@gmail.com>
2024-02-01 17:43:47 +0900

    カレンダーの日付を出力

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,7 +6,7 @@ class Calendar
   DAY_OF_WEEK = 'Su Mo Tu We Th Fr Sa'
   PRINT_WIDTH = DAY_OF_WEEK.length
 
-  attr_reader :target_date
+  attr_reader :target_date, :first_day, :last_day
 
   def initialize(target_date = Date.today)
     @target_date = target_date
@@ -14,6 +14,7 @@ class Calendar
 
   def print_calendar
     print_headers
+    print_numbers
   end
 
   private
@@ -21,6 +22,18 @@ class Calendar
   def print_headers
     puts target_date.strftime('%B %Y').center(PRINT_WIDTH)
     puts DAY_OF_WEEK
+  end
+
+  def print_numbers
+    first_day = Date.new(target_date.year, target_date.month, 1)
+    last_day = Date.new(target_date.year, target_date.month, -1)
+
+    print '   ' * first_day.wday
+    first_day.step(last_day) do |date|
+      print date.day.to_s.rjust(2, ' '), ' '
+      puts if date.saturday?
+    end
+    puts
   end
 end
 
