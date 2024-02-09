2865d26a7bfc14bf0899078a101840c0a3f97b42
paulo0212 <rikuo.iwasaki@gmail.com>
2024-02-09 19:53:15 +0900

    ファイル一覧を3列で出力するように変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,9 +9,18 @@ def main
   list_files(ordered_files)
 end
 
-def list_files(files)
-  files.each { |file| print file, ' ' }
-  print "\n"
+def list_files(files, cols: 3)
+  rows = files.size.ceildiv(cols)
+
+  rows.times do |row_num|
+    cols.times do |col_num|
+      num = row_num + col_num * rows
+      break if num >= files.size
+
+      print files[num].ljust(20)
+    end
+    print "\n"
+  end
 end
 
 main
