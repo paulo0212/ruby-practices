f30a6eb406b70c773456d38e1a757fabea7ce586
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-18 04:26:31 +0900

    ファイル指定 && オプションありパターンを実装

--- 05.wc/bin/wc.rb
+++ 05.wc/bin/wc.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 require 'pathname'
 require './lib/wc_cmd'
 
+opt = OptionParser.new
+
+options = { lines: false, words: false, chars: false }
+opt.on('-l') { |v| options[:lines] = v }
+opt.on('-w') { |v| options[:words] = v }
+opt.on('-c') { |v| options[:chars] = v }
+opt.parse!(ARGV)
 pathnames = ARGV.map { |path| Pathname(path) }
 
-puts main(pathnames)
+puts main(pathnames, **options)
--- 05.wc/lib/wc_cmd.rb
+++ 05.wc/lib/wc_cmd.rb
@@ -2,37 +2,45 @@
 
 require 'shellwords'
 
-def main(pathnames)
-  count_data = pathnames.map { |p| build_wc_data(p) }
-  count_rows = count_data.map { |d| format_row(**d) }
-  if pathnames.size > 1
-    total_data = build_total_data(count_data)
-    total_row = format_row(**total_data)
-  end
-  [count_rows, total_row].join("\n")
+def main(pathnames, lines: true, words: true, chars: true)
+  options = manage_options(lines, words, chars)
+  count_data = pathnames.map { |p| build_wc_data(p, **options) }
+  count_data << build_total_data(count_data) if pathnames.count > 1
+  count_data.map { |d| format_row(**d) }.join("\n")
 end
 
-def build_wc_data(pathname)
+def manage_options(lines, words, chars)
+  all_params_equal = lines == words && words == chars
+  return { lines: true, words: true, chars: true } if all_params_equal
+
+  { lines:, words:, chars: }
+end
+
+def build_wc_data(pathname, lines: true, words: true, chars: true)
   # TODO: 異常系も実装する
   # 指定パスが存在しない場合 -> `open: No such file or directory`
   # 指定パスがディレクトリの場合 -> `read: Is a directory`
   file = File.open(pathname, &:read)
-  lines = file.lines.count
-  words = file.split(/\s+/).count
-  chars = file.bytesize
-  { label: pathname, counts: { lines:, words:, chars: } }
-end
-
-def format_row(label:, counts:)
-  lines = counts[:lines].to_s.rjust(8, ' ') || ''
-  words = counts[:words].to_s.rjust(8, ' ') || ''
-  chars = counts[:chars].to_s.rjust(8, ' ') || ''
-  [lines, words, chars, " #{label}"].join
+  lines = lines ? file.lines.count : nil
+  words = words ? file.split(/\s+/).count : nil
+  chars = chars ? file.bytesize : nil
+  { label: pathname, counts: { lines:, words:, chars: }.compact }
 end
 
 def build_total_data(data)
-  lines = data.sum { |d| d[:counts][:lines] }
-  words = data.sum { |d| d[:counts][:words] }
-  chars = data.sum { |d| d[:counts][:chars] }
-  { label: 'total', counts: { lines:, words:, chars: } }
+  lines = data.sum { |d| d[:counts][:lines] } if data.first[:counts].key?(:lines)
+  words = data.sum { |d| d[:counts][:words] } if data.first[:counts].key?(:words)
+  chars = data.sum { |d| d[:counts][:chars] } if data.first[:counts].key?(:chars)
+  { label: 'total', counts: { lines:, words:, chars: }.compact }
+end
+
+def format_row(label:, counts:)
+  lines = format_count(counts[:lines])
+  words = format_count(counts[:words])
+  chars = format_count(counts[:chars])
+  [lines, words, chars, " #{label}"].join
+end
+
+def format_count(count)
+  count ? count.to_s.rjust(8, ' ') : ''
 end
