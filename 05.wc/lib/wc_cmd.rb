1fe85a5a12499b44c197f7de0c36960ab213b479
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-18 22:54:00 +0900

    標準入力パターンを実装

--- 05.wc/lib/wc_cmd.rb
+++ 05.wc/lib/wc_cmd.rb
@@ -4,7 +4,7 @@ require 'shellwords'
 
 def main(pathnames, lines: true, words: true, chars: true)
   options = manage_options(lines, words, chars)
-  count_data = pathnames.map { |p| build_wc_data(p, **options) }
+  count_data = pathnames.count.zero? ? wc_stdin(**options) : wc_files(pathnames, **options)
   count_data << build_total_data(count_data) if pathnames.count > 1
   count_data.map { |d| format_row(**d) }.join("\n")
 end
@@ -16,15 +16,25 @@ def manage_options(lines, words, chars)
   { lines:, words:, chars: }
 end
 
-def build_wc_data(pathname, lines: true, words: true, chars: true)
-  # TODO: 異常系も実装する
-  # 指定パスが存在しない場合 -> `open: No such file or directory`
-  # 指定パスがディレクトリの場合 -> `read: Is a directory`
-  file = File.open(pathname, &:read)
-  lines = lines ? file.lines.count : nil
-  words = words ? file.split(/\s+/).count : nil
-  chars = chars ? file.bytesize : nil
-  { label: pathname, counts: { lines:, words:, chars: }.compact }
+def wc_stdin(lines: true, words: true, chars: true)
+  [build_wc_data($stdin.read, lines:, words:, chars:)]
+end
+
+def wc_files(pathnames, lines: true, words: true, chars: true)
+  pathnames.map do |pathname|
+    # TODO: 異常系も実装する
+    # 指定パスが存在しない場合 -> `open: No such file or directory`
+    # 指定パスがディレクトリの場合 -> `read: Is a directory`
+    file = File.open(pathname, &:read)
+    build_wc_data(file, label: pathname, lines:, words:, chars:)
+  end
+end
+
+def build_wc_data(contents, label: nil, lines: true, words: true, chars: true)
+  lines = lines ? contents.lines.count : nil
+  words = words ? contents.split(/\s+/).count : nil
+  chars = chars ? contents.bytesize : nil
+  { label:, counts: { lines:, words:, chars: }.compact }
 end
 
 def build_total_data(data)
@@ -38,7 +48,8 @@ def format_row(label:, counts:)
   lines = format_count(counts[:lines])
   words = format_count(counts[:words])
   chars = format_count(counts[:chars])
-  [lines, words, chars, " #{label}"].join
+  label = " #{label}" if label
+  [lines, words, chars, label].join
 end
 
 def format_count(count)
