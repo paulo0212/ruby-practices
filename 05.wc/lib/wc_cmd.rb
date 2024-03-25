848579e8c7a56ac9e1b057c1220bbb96a4ec2df2
paulo0212 <rikuo.iwasaki@gmail.com>
2024-03-25 15:26:07 +0900

    キーワード引数からデフォルト値を削除

--- 05.wc/lib/wc_cmd.rb
+++ 05.wc/lib/wc_cmd.rb
@@ -13,11 +13,11 @@ def manage_options(lines, words, chars)
   { lines:, words:, chars: }
 end
 
-def wc_stdin(lines: true, words: true, chars: true)
-  [build_wc_data($stdin.read, lines:, words:, chars:)]
+def wc_stdin(lines:, words:, chars:)
+  [build_wc_data($stdin.read, label: nil, lines:, words:, chars:)]
 end
 
-def build_wc_data(contents, label: nil, lines: true, words: true, chars: true)
+def build_wc_data(contents, label:, lines:, words:, chars:)
   if contents
     lines_cnt = lines ? contents.lines.count : nil
     words_cnt = words ? contents.split(/\s+/).count : nil
@@ -26,13 +26,13 @@ def build_wc_data(contents, label: nil, lines: true, words: true, chars: true)
   { label:, lines_cnt:, words_cnt:, chars_cnt: }
 end
 
-def wc_files(pathnames, lines: true, words: true, chars: true)
+def wc_files(pathnames, lines:, words:, chars:)
   count_data = pathnames.map { |p| wc_file(p, lines:, words:, chars:) }
   count_data << build_total_data(count_data, lines:, words:, chars:) if pathnames.count > 1
   count_data
 end
 
-def wc_file(pathname, lines: true, words: true, chars: true)
+def wc_file(pathname, lines:, words:, chars:)
   file = read_file(pathname)
   build_wc_data(file[:contents], label: file[:label], lines:, words:, chars:)
 end
@@ -44,14 +44,14 @@ def read_file(pathname)
   { contents: File.open(pathname, &:read), label: pathname }
 end
 
-def build_total_data(data, lines: true, words: true, chars: true)
+def build_total_data(data, lines:, words:, chars:)
   lines_cnt = data.sum { |d| d[:lines_cnt].to_i } if lines
   words_cnt = data.sum { |d| d[:words_cnt].to_i } if words
   chars_cnt = data.sum { |d| d[:chars_cnt].to_i } if chars
   { label: 'total', lines_cnt:, words_cnt:, chars_cnt: }
 end
 
-def format_row(label: nil, lines_cnt: nil, words_cnt: nil, chars_cnt: nil)
+def format_row(label:, lines_cnt:, words_cnt:, chars_cnt:)
   counts = [lines_cnt, words_cnt, chars_cnt]
   counts_str = counts.map { |cnt| cnt ? cnt.to_s.rjust(8, ' ') : '' }
   counts_exist = counts.any?(Integer)
