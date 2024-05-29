28c3a73bb5525a8ab4712982b74dde6a4966dfe8
paulo0212 <rikuo.iwasaki@gmail.com>
2024-05-29 12:28:23 +0900

    Gameクラスを実装

---
+++ 06.bowling_object/lib/game.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+require_relative 'shot'
+
+class Game
+  def initialize(score_marks)
+    shots = parse_score_marks(score_marks)
+    @frames = build_frames(shots)
+  end
+
+  def score
+    @frames.map.with_index do |frame, idx|
+      if idx < 9
+        frame.score + bonus_score(@frames, idx) # 1st to 9th frame
+      else
+        frame.score # 10th frame
+      end
+    end.sum
+  end
+
+  private
+
+  def parse_score_marks(score_marks)
+    score_marks.split(',').map { |sm| Shot.new(sm) }
+  end
+
+  def build_frames(shots)
+    frames = []
+    current_frame = []
+
+    shots.each do |shot|
+      current_frame << shot
+      next if frames.size == 9
+
+      if shot.score == 10 || current_frame.size == 2
+        frames << Frame.new(current_frame) # 1st to 9th frame
+        current_frame = []
+      end
+    end
+    frames << Frame.new(current_frame) # 10th frame
+    frames
+  end
+
+  def bonus_score(frames, idx)
+    current_frame = frames[idx]
+
+    if current_frame.strike?
+      next_two_frames = [frames[idx + 1]&.shots, frames[idx + 2]&.shots].flatten
+      next_two_frames.first(2).sum(&:score)
+    elsif current_frame.spare?
+      next_shot = frames[idx + 1].shots.first
+      next_shot.score
+    else
+      0
+    end
+  end
+end
