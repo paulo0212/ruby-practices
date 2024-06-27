6ee4e026d5d6c5cf0b46ab7912fda3a7d221799a
paulo0212 <rikuo.iwasaki@gmail.com>
2024-06-27 13:26:06 +0900

    ShortFormatterを実装

--- 07.ls_object/lib/entry.rb
+++ 07.ls_object/lib/entry.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Entry
+  attr_reader :name
+
   def initialize(path)
     @pathname = Pathname.new(path)
+    @name = File.basename(path)
   end
 end
--- 07.ls_object/lib/short_formatter.rb
+++ 07.ls_object/lib/short_formatter.rb
@@ -1,19 +1,40 @@
 # frozen_string_literal: true
 
+require 'io/console'
 require_relative './base_formatter'
 
 class ShortFormatter < BaseFormatter
+  PADDING = 1
+
+  def initialize
+    super
+    @win_size = IO.console.winsize[1]
+  end
+
   def format_entries(entries, reverse: false)
-    target_entries = reverse ? entries.reverse : entries
-    puts '= = = = = Short Formatter = = = = ='
-    target_entries.map do |e|
-      format_entry(e)
+    entries = reverse ? entries.reverse : entries
+
+    @col_size = entries.map { |entry| entry.name.length }.max + PADDING
+    cols = @win_size / @col_size
+    rows = entries.count.ceildiv(cols)
+
+    transposed_entries = safe_transpose(entries.each_slice(rows).to_a)
+    transposed_entries.map do |entries_row|
+      format_row(entries_row)
     end
   end
 
   private
 
+  def safe_transpose(nested_entries)
+    nested_entries[0].zip(*nested_entries[1..])
+  end
+
+  def format_row(entries)
+    entries.map { |entry| format_entry(entry) }.join.rstrip
+  end
+
   def format_entry(entry)
-    puts "formatted_#{entry.inspect}"
+    entry&.name&.ljust(@col_size)
   end
 end
